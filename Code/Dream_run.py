a11e0587c32df5aa4e0b067172688fba8a9f9b8e
pshivraj <shivrajpurshottam@gmail.com>
2018-01-23 18:28:28 +0000

    Adding Dream run class

--- Code/Dream_run.py
+++ Code/Dream_run.py
@@ -18,3 +18,20 @@ def change_setting(config_file, setting_name, new_value, occurrence_loc='g'):
                              , occurrence_loc = occurrence_loc)
     return subprocess.call(sed_cmd, shell=True)
 
+#Files and their input paths
+config_file = 'Input.sauk.Livneh2013.dynG.rbm'
+streamflow_only = 'output/Streamflow.Only'
+validation_csv = 'validation.csv'
+dhsvm_cmd = 'DHSVM3.1.3 ' + config_file
+
+class Dream_run_setup(object):
+    def __init__(self):
+
+        self.params = [spotpy.parameter.Uniform('Lateral_Conductivity',low=0.001000 , high=0.003000,  optguess=0.001500),
+                       spotpy.parameter.Uniform('Depth_Threshold',low=0.0200000000 , high=0.0900000000,  optguess=0.0500000000),
+                       spotpy.parameter.Uniform('Maximum_Infiltration',low=0.010 , high=0.020,  optguess=0.017)
+                       ]
+        evaluation = pd.read_csv(validation_csv)
+        self.evals = evaluation['value'].values                   
+        print(len(self.evals))
+
